93ca63b72d1b893affb21d46c26803ebbfa55136
Bill <billcheptoyek60@gmail.com>
2025-08-04 12:48:11 +0300

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleTaskManager.Models;
+
+namespace SimpleTaskManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
